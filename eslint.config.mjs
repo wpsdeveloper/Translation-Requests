15248892abb2346c13f4e53ade00da09e9875392
Tom Reeve <tomreevetech@gmail.com>
2026-05-10 00:26:02 -0400

    set eslint to ignore dist folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import js from "@eslint/js";
 import globals from "globals";
 
 export default [
+  {
+    ignores: ["dist/**"],
+  },
   js.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,gs}"],
